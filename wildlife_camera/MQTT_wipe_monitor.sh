914048c0ad23c27189c6b61a86c522465533f54d
Kas-ES <55737559+Kas-ES@users.noreply.github.com>
2024-05-21 13:03:29 +0200

    added_rainsensor

---
+++ wildlife_camera/MQTT_wipe_monitor.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+MQTT_HOST=${1:-"mqtt://mqtt-dashboard.com"}
+MQTT_TOPIC_COMMAND=${2:-"sdu/2024/emuli/group04/itrains"}
+MQTT_TOPIC_LISTEN=${3:-"sdu/2024/emuli/group04/rain_sensor"}
+
+while true  # Keep an infinite loop to reconnect when connection lost/broker unavailable
+do
+    mosquitto_sub -h $MQTT_HOST -t $MQTT_TOPIC_COMMAND | while read -r payload
+    do
+        # Check if there is rain
+        if [ $payload -eq 1 ]; then
+            mosquitto_pub -h $MQTT_HOST -t $MQTT_TOPIC_LISTEN -m "trigger wiper"
+        fi
+    done
+    sleep 5  # Wait 10 seconds until reconnection
+done # &  # Discomment the & to run in background (but you should rather run THIS script in background)
---
+++ wildlife_camera/rain_wiper.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Change your device accordinly to the usb port.
+DEVICE=${1:-"/dev/ttyAIO"}
+MQTT_HOST=${2:-"mqtt://mqtt-dashboard.com"}
+MQTT_TOPIC_COMMAND=${3:-"sdu/2024/emuli/group04/itrains"}
+MQTT_TOPIC_LISTEN=${4:-"sdu/2024/emuli/group04/rain_sensor"}
+
+change_wipe() {
+    angle="$1"
+    if [ -c $DEVICE ]; then
+        JSON= "{\"wiper_angle\": $angle}"
+        echo $JSON > $DEVICE
+    fi
+    echo "wiping potentially done"
+}
+
+while true  # Keep an infinite loop to reconnect when connection lost/broker unavailable
+do
+
+    # Read JSON message from serial port
+    json_message=$(cat "$serial_port" | jq -c .)
+
+    rain_detect_value=$(echo "$json_message" | jq -r '.rain_detect')
+    
+
+    if [ "$rain_detect_value" -eq 1 ]; then
+        # Publish message to MQTT broker
+        mosquitto_pub -h $MQTT_HOST -t $MQTT_TOPIC_COMMAND -m $rain_detect_value
+    fi
+
+    mosquitto_sub -h $MQTT_HOST -t $MQTT_TOPIC_LISTEN | while read -r payload
+    do
+        # Here is the callback to execute whenever you receive a message:
+        echo "Rx MQTT: ${payload}"
+        # Control the rain wiper.
+        change_wipe 180
+        echo "wiping 180"
+        sleep 2
+        control_wipe 0
+        echo "wiping 0"
+    done
+    sleep 5  # Wait 10 seconds until reconnection
+done # &  # Discomment the & to run in background (but you should rather run THIS script in background)
