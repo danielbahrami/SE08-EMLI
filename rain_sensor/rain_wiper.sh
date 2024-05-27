4d37914698f751ddc434e2526ee33ca1180f8973
Kas-ES <55737559+Kas-ES@users.noreply.github.com>
2024-05-27 12:12:54 +0200

    removed mqtt://

--- rain_sensor/rain_wiper.sh
+++ rain_sensor/rain_wiper.sh
@@ -2,7 +2,7 @@
 
 # Change your device accordinly to the usb port.
 DEVICE=${1:-"/dev/ttyAIO"}
-MQTT_HOST=${2:-"mqtt://mqtt-dashboard.com"}
+MQTT_HOST=${2:-"mqtt-dashboard.com"}
 MQTT_TOPIC_COMMAND=${3:-"org/sdu/2024/emli/group04/wilddrone/rain/command"}
 MQTT_TOPIC_RESPONSE=${4:-"org/sdu/2024/emli/group04/wilddrone/rain/response"}
 
@@ -29,12 +29,12 @@ while true; do # Keep an infinite loop to reconnect when connection lost/broker 
 
     mosquitto_sub -h $MQTT_HOST -t $MQTT_TOPIC_RESPONSE | while read -r payload; do
         # Here is the callback to execute whenever you receive a message:
-        echo "Rx MQTT: ${payload}"
+        echo "Beging wiping: ${payload}"
         # Control the rain wiper.
         change_wipe 180
         echo "wiping 180 degrees"
         sleep 2
-        control_wipe 0
+        change_wipe 0
         echo "wiping 0 degrees"
     done
     sleep 5 # Wait 5 seconds until reconnection
