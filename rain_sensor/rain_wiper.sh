dd4c5bb254bf3b77a9757d12f60ca232a135ade8
Kas-ES <55737559+Kas-ES@users.noreply.github.com>
2024-05-27 12:21:20 +0200

    added read_serial.sh

--- rain_sensor/rain_wiper.sh
+++ rain_sensor/rain_wiper.sh
@@ -3,8 +3,7 @@
 # Change your device accordinly to the usb port.
 DEVICE=${1:-"/dev/ttyAIO"}
 MQTT_HOST=${2:-"mqtt-dashboard.com"}
-MQTT_TOPIC_COMMAND=${3:-"org/sdu/2024/emli/group04/wilddrone/rain/command"}
-MQTT_TOPIC_RESPONSE=${4:-"org/sdu/2024/emli/group04/wilddrone/rain/response"}
+MQTT_TOPIC_RESPONSE=${3:-"org/sdu/2024/emli/group04/wilddrone/rain/response"}
 
 change_wipe() {
     degrees="$1"
@@ -16,20 +15,9 @@ change_wipe() {
 }
 
 while true; do # Keep an infinite loop to reconnect when connection lost/broker unavailable
-
-    # Read JSON message from serial port
-    json_message=$(cat "$DEVICE" | jq -c .)
-
-    rain_detect_value=$(echo "$json_message" | jq -r '.rain_detect')
-
-    if [ $rain_detect_value -eq 1 ]; then
-        # Publish message to MQTT broker
-        mosquitto_pub -h $MQTT_HOST -t $MQTT_TOPIC_COMMAND -m $rain_detect_value
-    fi
-
     mosquitto_sub -h $MQTT_HOST -t $MQTT_TOPIC_RESPONSE | while read -r payload; do
         # Here is the callback to execute whenever you receive a message:
-        echo "Beging wiping: ${payload}"
+        echo "Rx MQTT: ${payload}"
         # Control the rain wiper.
         change_wipe 180
         echo "wiping 180 degrees"
