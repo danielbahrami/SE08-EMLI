ad2004004468713e106db850a07906055fe29332
Daniel Bahrami <daniel@bahrami.dk>
2024-05-28 10:51:50 +0200

    Update annotation.sh

--- WildDrone/cloud/annotation.sh
+++ WildDrone/cloud/annotation.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Directory containing the images
-IMAGE_DIR=${1:-"/path/to/your/images"}
+IMAGE_DIR=${1:-"/path/to/images"}
 AUTHOR_NAME=${2:-"Name"}
 AUTHOR_EMAIL=${3:-"email@example.com"}
-# Ollama endpoint - follow the below commands
-# $ ollama serve
-# $ ollama pull llava ; if not already pulled 
+
+# Set up Ollama
+ollama serve
+ollama pull llava
 GENERATEENDPOINT=${4:-"http://localhost:11434/api/generate"}
 
 CHECK_ANNOTATION_STATUS=0
@@ -14,18 +15,17 @@ CHECK_ANNOTATION_STATUS=0
 while true; do
   # Loop through each image in the directory
   for img in "$IMAGE_DIR"*.{jpg,png}; do
-    # Check if the file exists to handle cases where no files match the pattern
+    # Check if the file exist
     if [[ -f "$img" ]]; then
-      # Process the image (e.g., print the file name)
       echo "img path: $img"
 
-      # Get the filename without extension
+      # Get the file name without an extension
       filename=$(basename "${img%.*}")
 
       # Construct the corresponding JSON file path
       json_file="$IMAGE_DIR$filename.json"
 
-      if jq -e '.Annotation' "$json_file" > /dev/null; then
+      if jq -e '.Annotation' "$json_file" >/dev/null; then
         echo "Skipping already annotated image: $image_file"
         continue
       fi
@@ -34,8 +34,7 @@ while true; do
       # Setting it to 0 means there will be no line breaks in the output, resulting in a single continuous line of base64-encoded data
       img_base64=$(base64 -w 0 -i $img)
 
-      ### Ollama send ###
-      # Payload for the Ollama
+      # Payload for Ollama
       JSON_MSG="{\"model\": \"llava\", \"prompt\": \"describe this image shortly\", \"stream\": false, \"images\": [\"$img_base64\"]}"
 
       # Send the JSON payload to Ollama's generate endpoint using curl
@@ -43,25 +42,26 @@ while true; do
         -H "Content-Type: application/json" \
         -d "$JSON_MSG")
 
-      #Check if failed
+      # Failure check
       if [ $? -ne 0 ]; then
-        echo "Annotating $filename failed... Check internet"
+        echo "Annotating $filename failed"
         continue
       fi
 
-      # Extract the description response from curl command
+      # Extract the description response from curl
       response=$(echo "$curl_response" | jq -r '.response')
+
       # Create the JSON annotation
       ANNOTATION_JSON="{\"Source\": \"llava\", \"Test\": \"$response\"}"
 
-      # Write the newly created JSON annotation to the json file      
-      jq --argjson Annotation "$ANNOTATION_JSON" '.+={$Annotation}' "$json_file" > tmp.json && mv tmp.json "$json_file"
+      # Write the newly created JSON annotation to the json file
+      jq --argjson Annotation "$ANNOTATION_JSON" '.+={$Annotation}' "$json_file" >tmp.json && mv tmp.json "$json_file"
 
       $CHECK_ANNOTATION_STATUS=1
     fi
   done
 
-  # directss upload.sh
+  # Run upload.sh
   if [ $CHECK_ANNOTATION_STATUS -eq 1 ]; then
     echo "Uploading annotated files"
     ./upload.sh $IMAGE_DIR $USER_NAME $USER_EMAIL
