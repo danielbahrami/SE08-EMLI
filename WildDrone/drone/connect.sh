8d920ae1f02090e08485807b4fd455948de7d5b7
Marc Bertelsen <71696960+Wafl97@users.noreply.github.com>
2024-05-29 20:42:30 +0200

    made drone script run infinitely

--- WildDrone/drone/connect.sh
+++ WildDrone/drone/connect.sh
@@ -8,30 +8,32 @@ cam_home="simonplatz@10.0.0.10"
 cam_photo_path="$cam_home:SE08-EMLI/WildDrone/wildlife_photos"
 cam_photo_pass="simonplatz"
 
-echo "Looking for cam"
-# Find Wi-Fi
 while true; do
-    if nmcli -f SSID device wifi | grep -q $cam_ssid; then
-        break
-    fi
+    echo "Looking for cam"
+    # Find Wi-Fi
+    while true; do
+        if nmcli -f SSID device wifi | grep -q $cam_ssid; then
+            break
+        fi
+    done
+    echo "Found cam"
+    echo "Connecting..."
+    # Connect
+    nmcli dev wifi connect $cam_ssid password $cam_wifi_pass
+    echo "Connected"
+
+    # Sync time
+    echo "Sync time"
+    sshpass -p $cam_pass ssh $cam_home sudo date --set @$(date -u +%s)
+
+    chmod 777 ./log_wifi.sh
+    chmod 777 ./save_files.sh
+    ./log_wifi.sh &
+    cat ./save_files.sh | sshpass -p $cam_pass ssh $cam_home
+
+    sshpass -p $cam_photo_pass rsync -a --ignore-existing $cam_photo_path ./
+
+    nmcli con down $cam_ssid
+
+    sleep 60
 done
-echo "Found cam"
-echo "Connecting..."
-# Connect
-nmcli dev wifi connect $cam_ssid password $cam_wifi_pass
-echo "Connected"
-
-# Sync time
-echo "Sync time"
-sshpass -p $cam_pass ssh $cam_home sudo date --set @$(date -u +%s)
-
-chmod 777 ./log_wifi.sh
-chmod 777 ./save_files.sh
-./log_wifi.sh &
-cat ./save_files.sh | sshpass -p $cam_pass ssh $cam_home
-
-sshpass -p $cam_photo_pass rsync -a --ignore-existing $cam_photo_path ./
-
-sleep 5
-
-nmcli con down $cam_ssid
